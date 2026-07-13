eb74174c302fe4b2fcac2a05ed06104ff2d23114
hummingbird09 <167159194+hummingbird09@users.noreply.github.com>
2026-07-13 22:15:03 +0530

    Time: 0 ms (100%), Space: 44 MB (16.75%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.java
@@ -0,0 +1,22 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int iIndex =  0 ;
+        int n = matrix.length;
+        int[][] copy = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                copy[i][j] = matrix[i][j];
+            }
+        }
+
+        for(int i = 0 ; i<matrix.length ; i++){
+            int jIndex = 0;
+            for(int j = matrix.length-1 ; j>=0 ; j--){
+             matrix[iIndex][jIndex] = copy[j][i];
+                jIndex++;
+            }
+            iIndex ++;
+
+        }
+    }
+}
